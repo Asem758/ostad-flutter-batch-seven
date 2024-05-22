2d35e92dd2dd967286051f6f956d58759ae96a2d
Ashrafur Rahman Asem <ashrafurasem@gmail.com>
2024-05-22 12:59:02 +0600

    added files

---
+++ bin/ostad_flutter_batch_7.dart
@@ -0,0 +1,11 @@
+main(){
+  print('Hello Dart');
+  print(8473572);
+  print('Hello Dart');
+  print(8473572);
+
+  print(12*76);
+  print(12 + 54);
+  print(34 / 98);
+  print(34 % 34);
+}
---
+++ bin/variables.dart
@@ -0,0 +1,54 @@
+main(){
+  //primitive data type
+  //String(Textual Data), integer(int), double(floating value), boolean(bool)
+
+  /*print('user\'name');
+  print("'log\'in'");
+  print('But\'ton''');
+  
+  print(95784753.runtimeType);
+  print(893.2812.runtimeType);
+
+  //bool
+  print(true);
+  print(true.runtimeType);
+*/
+  // variable is container--> Contains a value
+  // data type -> Variable name = value
+
+  String userName = 'Ashrafur Rahman Asem';
+
+  print(userName);
+  print(userName);
+  print(userName);
+  print(userName);
+
+  userName = 'Asem';
+  print(userName);
+  userName = 'john';
+  print(userName);
+
+  int age = 34;
+  print(age);
+  age = 56;
+  print(age);
+
+  double cgpa = 3.45;
+  print(double);
+  cgpa = 3.79;
+  print(cgpa);
+
+  bool isSuccess = false;
+  print(isSuccess);
+
+  isSuccess = true;
+  print(isSuccess);
+
+  //CamelCase****
+  //=> myName
+  //Snack_Case
+  //Pascal
+  //SCREAMING
+
+
+}
